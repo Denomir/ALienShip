f4bd14577fc5ffde5dd4ced483301bd22b777751
Denom <baphomier@gmail.com>
2026-02-25 14:40:51 +0200

    Add bullet configuration

--- settings.py
+++ settings.py
@@ -5,3 +5,7 @@ class Settings:
         # self.bg_color = (100,101,110)
         self.bg_color = (135,206,235)
         self.ship_speed = 10
+        self.bullet_speed = 2.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60,60,60)

27adabf5f082bcf30c5a7427d45a0dc09e7bb23e
Denom <baphomier@gmail.com>
2026-02-25 14:53:57 +0200

    end work with game

--- alien_invasion.py
+++ alien_invasion.py
@@ -64,7 +64,7 @@ class AlienInvasion:
         self.ship.blitme()
         self.person.blitme()
         pygame.display.flip()
-        
+         
 if __name__ == '__main__':
     ai = AlienInvasion()
     ai.run_game()
